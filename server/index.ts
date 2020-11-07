f137d23d2b58cceaa72ef8a68d0914ef47c8f7a1
Timo Kerola <timo.kerola777@gmail.com>
2020-11-07 18:54:45 +0200

    refactor to a class midway

--- server/index.ts
+++ server/index.ts
@@ -1,47 +1,19 @@
 import { app } from "./src/app";
-import {
-  initializeAvailabilityData,
-  initializeProductData,
-  initializeData,
-} from "./src/fetchData/fetchData";
+import { ProductDataFetcher } from "./src/fetchData/fetchData";
 import cron from "node-cron";
 
 const port = process.env.PORT || 3001;
 
-export interface ProductData {
-  id: string;
-  name: string;
-  price: number;
-  manufacturer: string;
-  type: string;
-  availability?: string;
-}
+// export let jackets: ProductData[];
+// export let shirts: ProductData[];
+// export let accessories: ProductData[];
+// export let manufacturerLookupObj: ManufacturerLookupObject;
 
-export interface ManufacturerLookupObject {
-  [id: string]: { availability: string; manufacturer: string };
-}
+export const productDataFetcher = new ProductDataFetcher();
 
-export let jackets: ProductData[];
-export let shirts: ProductData[];
-export let accessories: ProductData[];
-export let manufacturerLookupObj: ManufacturerLookupObject;
-
-initializeData().then((data) => {
-  const { productData, availabilityData } = data;
-
-  jackets = productData.jackets;
-  shirts = productData.shirts;
-  accessories = productData.accessories;
-
-  manufacturerLookupObj = availabilityData;
-
-  cron.schedule("*/5 * * * *", async () => {
-    const data = await initializeData();
-    jackets = data.productData.jackets;
-    shirts = data.productData.shirts;
-    accessories = data.productData.accessories;
-
-    manufacturerLookupObj = data.availabilityData;
+productDataFetcher.initializeData().then(() => {
+  cron.schedule("*/5 * * * *", (): void => {
+    productDataFetcher.initializeData();
     console.log("Fetching data...");
   });
 
@@ -49,3 +21,27 @@ initializeData().then((data) => {
     console.log(`Listening on port ${port}`);
   });
 });
+
+// initializeData().then((data) => {
+//   const { productData, availabilityData } = data;
+
+//   jackets = productData.jackets;
+//   shirts = productData.shirts;
+//   accessories = productData.accessories;
+
+//   manufacturerLookupObj = availabilityData;
+
+//   cron.schedule("*/5 * * * *", async () => {
+//     const data = await initializeData();
+//     jackets = data.productData.jackets;
+//     shirts = data.productData.shirts;
+//     accessories = data.productData.accessories;
+
+//     manufacturerLookupObj = data.availabilityData;
+//     console.log("Fetching data...");
+//   });
+
+//   app.listen(port, () => {
+//     console.log(`Listening on port ${port}`);
+//   });
+// });
