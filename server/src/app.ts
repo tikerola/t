b537461586bf9488ee45b9778b7bfde9e2707a98
Timo Kerola <timo.kerola777@gmail.com>
2020-11-15 23:56:56 +0200

    app refresh fix

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import { accessoryRouter } from "./routes/accessories";
 import { jacketsRouter } from "./routes/jackets";
 import { shirtsRouter } from "./routes/shirts";
+import path from "path";
 
 const app = express();
 app.use(express.static("build"));
@@ -11,5 +12,9 @@ app.use(express.static("build"));
 app.use(jacketsRouter);
 app.use(shirtsRouter);
 app.use(accessoryRouter);
+
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, "../", "build", "index.html"));
+});
 
 export { app };
